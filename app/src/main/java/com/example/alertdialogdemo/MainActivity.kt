a8f983a1cb9d5df5fed06f8baaaba59ac2d73565
niraviman-singha <niraviman15-11689@diu.edu.bd>
2023-09-18 18:47:22 +0600

    Alert Dialog

---
+++ app/src/main/java/com/example/alertdialogdemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.alertdialogdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.alertdialogdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.alert.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("Delete Files")
+            dialog.setMessage("Do you want to delete files")
+            dialog.setIcon(R.drawable.baseline_delete_24)
+
+            dialog.setPositiveButton("Yes"){ dialogInterface, which->
+                Toast.makeText(this,"Deleted",Toast.LENGTH_SHORT).show()
+            }
+            dialog.setNegativeButton("No"){ dialogInterface, which->
+                Toast.makeText(this,"Click No",Toast.LENGTH_SHORT).show()
+            }
+            dialog.setNeutralButton("Cancel"){ dialogInterface, which->
+                Toast.makeText(this,"Click Cancel",Toast.LENGTH_SHORT).show()
+            }
+
+            val alertDialog:AlertDialog = dialog.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+    }
+}
